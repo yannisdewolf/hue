683cd59268e11630fbad525fe687cd8ffc4d979f
Yannis De Wolf <yannisdewolf@gmail.com>
2016-10-01 20:03:00 +0200

    property binding

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./databinding.component.css']
 })
 export class DatabindingComponent{
-  stringInterpolation = 'This is string interpolation';
+  stringInterpolation = 'This is string int';
   numberInterpolation = 1;
+
+  onTest() {
+    return true;
+  }
 
 }
